7b9b00105abe725a2c916332ebdb847c52520c40
Tuva-Li Wass Vallin <tuvali.wahlin@icloud.com>
2022-01-03 20:51:19 +0100

    started with cms layout

--- src/ts/models/Blog.ts
+++ src/ts/models/Blog.ts
@@ -1,1 +1,11 @@
-export class Blog {}
+export class Blog {
+  id: number;
+  name: string;
+  author: string;
+
+  constructor(id: number, name: string, authour: string) {
+    this.id = id;
+    this.name = name;
+    this.author = authour;
+  }
+}
